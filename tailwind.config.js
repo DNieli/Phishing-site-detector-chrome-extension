64278a29bab27c873e2cd8f53736a6fdab1f9035
DNieli <diegoantonio.nieli@gmail.com>
2026-03-07 16:31:41 +0000

    restructured file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["/popup-local.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./popup.html",
+    "./popup-local.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./chrome-extension/src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     react(),
     viteStaticCopy({
       targets: [
-        { src: "src/chrome-extension/manifest.json", dest: "." },
+        { src: "chrome-extension/public/manifest.json", dest: "." },
+        { src: "chrome-extension/public/icons", dest: "." },
       ],
     }),
   ],
@@ -17,13 +18,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         popup: resolve(__dirname, "popup.html"),
-        background: resolve(__dirname, "src/chrome-extension/background/index.ts"),
-        // content: resolve(__dirname, "src/chrome-extension/content/index.ts"),
+        background: resolve(__dirname, "chrome-extension/src/background/index.ts"),
       },
       output: {
         entryFileNames: (chunk) => {
           if (chunk.name === "background") return "background/index.js";
-        //   if (chunk.name === "content") return "content/index.js";
           return "[name].js";
         },
       },
