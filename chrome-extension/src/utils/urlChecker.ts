01c9ba1bdecd15c0f887d5d5f867f13cf8a0d0c5
DNieli <diegoantonio.nieli@gmail.com>
2026-03-22 12:38:28 +0000

    added cloudflare worker backend

--- chrome-extension/src/utils/urlChecker.ts
+++ chrome-extension/src/utils/urlChecker.ts
@@ -1,33 +1,7 @@
-import { VT_API_KEY } from "../config";
-
 export async function checkDomainForPhishing(url: string) {
-  const response = await fetch("https://www.virustotal.com/api/v3/urls", {
-    method: "POST",
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      "x-apikey": VT_API_KEY
-    },
-    body: `url=${encodeURIComponent(url)}`,
-  });
+  const response = await fetch(`https://phishing-site-detector-backend.diegoantonio-nieli.workers.dev?url=${encodeURIComponent(url)}`);
 
-  const data = await response.json();
-  console.log("VT URL SUBMISSION RESPONSE:", data);
-
-  if (!data.data?.id) {
-    console.error("VirusTotal did not return analysis ID:", data);
-    throw new Error("Missing analysis ID");
-  }
-
-  const analysisId = data.data.id;
-
-  const resultResponse = await fetch(
-    `https://www.virustotal.com/api/v3/analyses/${analysisId}`,
-    {
-      headers: { "x-apikey": VT_API_KEY }
-    }
-  );
-
-  const resultData = await resultResponse.json();
+  const resultData = await response.json();
   console.log("VT ANALYSIS RESULT:", resultData);
 
   return resultData;
